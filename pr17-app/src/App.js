da3eba20bdb3e5b76f69f3106d618eae9677885b
pablorosas17 <51768134+PabloRosas17@users.noreply.github.com>
2025-01-23 21:43:25 -0800

    deployed the site, now live. working repo as of Thu Jan 23 21:43:25 PST 2025

--- pr17-app/src/App.js
+++ pr17-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <Router basename="/timeline">
         <RoutesConfig />
       </Router>
     </ThemeProvider>
