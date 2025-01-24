45dc444e4f468b6baed9014f44dcfabdcdddbced
pablorosas17 <51768134+PabloRosas17@users.noreply.github.com>
2025-01-23 16:05:36 -0800

    put a flavorful taste to the lebeaute label with proper letters. working repo as of Thu Jan 23 16:05:36 PST 2025

--- pr17-app/src/assets/data/timelineData.js
+++ pr17-app/src/assets/data/timelineData.js
@@ -3,7 +3,7 @@ const timelineData = [
     { title: "Résumé", content: "This is my résumé. 📄" },
     { title: "PyFunzies", content: "🐑 PyFunzies is an ai data driven application that \
       improves effectiveness and efficiency of online ad targeting for an e-commerce platform" },
-    { title: "LeBeaute", content: "💕 LeBeauté es una aplicación dedicada al servicio de belleza." },
+    { title: "LeBéauté", content: "💕 LeBéauté es una aplicación dedicada al servicio de belleza." },
     { title: "SwiftTruck", content: "🚚 SwiftTruck is a SaaS application for mobile business. " },
     { title: "About", content: "About this website. ❓" },
 ];
