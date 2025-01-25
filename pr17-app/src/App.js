5db99292cda271e80812e2131db3735a93caa2f0
pablorosas17 <51768134+PabloRosas17@users.noreply.github.com>
2025-01-24 20:32:37 -0800

    adjusting deployment because of 404 issues. working repo as of Fri Jan 24 20:32:36 PST 2025
    - this should be on a test-branch but for now it goes directly, cause lazy

--- pr17-app/src/App.js
+++ pr17-app/src/App.js
@@ -1,18 +1,24 @@
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter as Router } from 'react-router-dom';
-import RoutesConfig from './routes/routes';
-import theme from './assets/theme/theme.js';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div color="#0f0f0f">DEBUG:APP.JS</div>
-      <Router basename="/pr17-app">
-        <RoutesConfig />
-      </Router>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
