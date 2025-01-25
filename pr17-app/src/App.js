b5383cc48c6cd073c455c0f679ee7a65a45343a0
pablorosas17 <51768134+PabloRosas17@users.noreply.github.com>
2025-01-24 16:10:10 -0800

    adjusting deployment because of 404 issues. working repo as of Fri Jan 24 16:10:10 PST 2025
    - this should be on a test-branch but for now it goes directly, cause lazy

--- pr17-app/src/App.js
+++ pr17-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename="/pr17/pr17-app" >
+      <Router >
         <RoutesConfig />
       </Router>
     </ThemeProvider>
